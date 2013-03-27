fe969019737fd2c64b0902f49f4673d659b816c0
Graeme Geldenhuys <graemeg@gmail.com>
2013-03-27 10:14:06 +0000

    Removed old batch & script files.
    * clear.bat is not needed, we can simply us 'git clean' instead
    * makehelp.* will be replaced with fpdoc based help. That way we can
      support HTML, CHM, PDF, INF etc help formats.

--- clear.bat
+++
@@ -1,8 +0,0 @@
-rd __history /s /q
-rd  lib /s /q
-del *.dcu
-del *.~*
-del *.o
-del *.ppu
-del *.bak
-del lib
--- makehelp.bat
+++
@@ -1,2 +0,0 @@
-pasdoc FBLDatabase.pas FBLTransaction.pas FBLDsql.pas FBLService.pas FBLExcept.pas FBLMetadata.pas FBLScript.pas FBLEvents.pas FBLSimple.pas FBLParamDsql.pas FBLDataset.pas -N FbLib -T FbLib-docs -E docs\html -O htmlhelp 
-pause
